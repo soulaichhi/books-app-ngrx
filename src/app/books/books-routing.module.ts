19392bdb036a5ebaf95162e7ae9009f0c9a47982
soulaichhi <chhibou.soulaimane@gmail.com>
2023-08-18 16:25:38 +0100

    Structure the project

--- src/app/books/books-routing.module.ts
+++ src/app/books/books-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
 
 const routes: Routes = [
-  {path:'',loadChildren:()=>import('./books/books.module').then(m=>m.BooksModule)}
+  {path:'',component:HomeComponent}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class BooksRoutingModule { }
